3ccd7d51fc22090b20957a58ecb34a7130c5a436
Harrison Fisher <HarrisonFisher2018@gmail.com>
2022-04-13 16:59:29 -0700

    Setup and login window

---
+++ ChatSystem/src/Client/ClientGui.java
@@ -0,0 +1,53 @@
+package Client;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientGui implements ClientUserInterface {
+	
+	public Boolean Login() {
+		
+		return false;
+	}
+	
+	
+	@Override
+	public void processCommands() {
+		// TODO Auto-generated method stub
+		
+		
+		while (true) {
+						
+			JTextField Username = new JTextField();
+			JTextField Password = new JPasswordField();
+			Object[] Message = {"Username:", Username,"Password:", Password};
+			int optionChoosen = JOptionPane.showConfirmDialog(null, Message, "Login", JOptionPane.OK_CANCEL_OPTION);
+			if (optionChoosen == JOptionPane.OK_OPTION) {
+				if (Login() == true) {
+					break;
+				}else {
+					
+					JFrame jFrame = new JFrame();
+			        String msg  = "Login Failed!";
+			        JOptionPane.showMessageDialog(jFrame, msg);
+					continue;
+				}
+	
+			}			
+			break;
+		}
+		
+		
+
+		
+		
+		
+		
+	}
+
+}
