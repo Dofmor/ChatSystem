0f7be3a740a5428eee24f059adf3571a124b5423
nickkhine <nickkhine@gmail.com>
2022-04-26 13:54:03 -0700

    Update Person.java

--- ChatSystem/src/Server/Person.java
+++ ChatSystem/src/Server/Person.java
@@ -1,26 +1,59 @@
 package Server;
 
 public class Person {
-	protected String name;
-	protected String username;
-	protected String password;
-	protected String userType;
+	private String username;
+	private String password;
+	private String userType;
+	private boolean loggedIn;
 	
-	public void login(String username, String password, String userType) {
-		//Calls user authentication from the server with the passed login variables
-		//userAuthentication(username, password, userType);
+	public Person(String username, String password, String userType) {
+		this.username = username;
+		this.password = password;
+		this.userType = userType;
+		this.loggedIn = false;
 	}
 	
-	public void sendMessage(String dataType, String data, String fromClient, String toClient, String fromServer, String toServer) {
-		//Sends messages with the entered in info
-		//Creates new message object
-		//Message message = new Message();
-		//message.type = dataType;
-		//message.data = data;
-		//message.fromClient = fromClient;
-		//message.toClient = toClient;
-		//message.fromServer = fromServer;
-		//message.toServer = toServer;
-		//sendCoversation(message);
+	public String getUsername() {
+		return username;
 	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	private void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+	
+	public void login(String username, String password) {
+		if(this.username == username && this.password == password) {
+			setLoggedIn(true);
+		}
+	}
+	
+	public void logout() {
+		setLoggedIn(false);
+	}
+	
+
 }
