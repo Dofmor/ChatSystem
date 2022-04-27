f349e9bb213973cafdd1a223e12f89d29cb4897b
bkl-13 <104529843+bkl-13@users.noreply.github.com>
2022-04-27 15:22:07 -0700

    Update Log.java

--- ChatSystem/src/Server/Log.java
+++ ChatSystem/src/Server/Log.java
@@ -4,55 +4,116 @@ import Shared.*;
 
 
 public class Log {
-	private Message m;
+		private Messages m;
 	/** this is to call from message /
 	 * 
 	 * @return
 	 */
 	private String[] ConversationData;
+	/**conversation data
+	 * 
+	 */
 	private String[] UserData;
 	
+	private String [][] ConversationDate= { (String[])  ConversationData};
+	
+	/**
+	 * 
+	 * This is the user data
+	 */
+	List<String> log;
+	String filename;
+	File file;
+
 	
 
 
 
 
-	public Log(Message m, String[] conversationData, String[] userData) {
+
+public Log(Messages m, String[] conversationData, String[] userData, String[][] conversationDate) {
 		this.m = m;
 		ConversationData = conversationData;
 		UserData = userData;
+		ConversationDate = conversationDate;
 	}
+/**
+void loadlog() {
+//open read file
+//store data from file to log
+}
+
+void addMessage() {
+//format message
+String formattedMsg = ""; //formatted message should be date, time, sender, receivers, message
+Messages m =new Messages(formattedMsg, formattedMsg, formattedMsg, formattedMsg, formattedMsg, formattedMsg);
+log.add(formattedMsg);
+m.getToServer();
+} 
 
 
+void saveLog() {
+	
+} //save everything in the log list to a file
 
+*/
 
-
-
-	/*
-	 * *this is to log message
-	 */
+/**this is to log message
+ 
+ */
 	public void LogMessage() {
-		if(m.getDate().isEmpty()) {
-			
-			while(m.getDate().contains(m.getFromClient())) {
-				System.out.println(m.getToServer());
-				
+		Messages m1= new Messages("", "", "", "", "", "");
+		Messages m2 = new Messages("", "", "", "", "", "");
+		String start = m1.getTime();
+        String end = m2.getTime();
+        String begin = m1.getDate();
+        String finale = m2.getDate();
+        
+		while(!m.getData().isEmpty()) {
+						
+	        for (int j = 0; j < ConversationData.length - 1; j++) {
+	 
+	            if (start.compareTo(end) < 0) {
+	 
+	                String temp = ConversationData[j];
+	                ConversationData[j] = ConversationData[j + 1];
+	                ConversationData[j + 1] = temp;
+	                j = -1;
+	            }
+	            
 			}
 			
-			
+	        for (int j = 0; j < ConversationDate.length - 1; j++) {
+	       	 
+	            if (begin.compareTo(finale) < 0) {
+	 
+	                String[] temp = ConversationDate[j];
+	                ConversationDate[j] = ConversationDate[j + 1];
+	                ConversationDate[j + 1] = temp;
+	                j = -1;
+	            }
 		}
+	        
+	        
+	        
+	        }
+		
+			/**addMessage();
+			 * 
+			 * 
+			 * */
+
+		
 			/** 
-			 * null is a place holder for message array
 			 * message will be organized by date of publish
 			 *  and if if message date list is empty then the array do nothing
-			 *  then if message date is full then 
 			 *  it go in a tranversal loop to reveal all the message said in that date
 			 *
 			 */
 			
 		}
 		
-		
+
 		
 	
 	
@@ -73,7 +134,7 @@ public class Log {
 
 
 
-	public Message getM() {
+	public Messages getM() {
 		return m;
 	}
 
@@ -82,7 +143,7 @@ public class Log {
 
 
 
-	public void setM(Message m) {
+	public void setM(Messages m) {
 		this.m = m;
 	}
 
@@ -121,7 +182,16 @@ public class Log {
 	public void setUserData(String[] userData) {
 		UserData = userData;
 	}
-
+	
 	
 
-}
+	public String[][] getConversationDate() {
+		return ConversationDate;
+	}
+
+	public void setConversationDate(String[][] conversationDate) {
+		ConversationDate = conversationDate;
+	}
+	
+
+	}
