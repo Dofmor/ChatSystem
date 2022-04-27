d4a4027018457f09a6baa6a6eab630583baf15dd
nickkhine <nickkhine@gmail.com>
2022-04-27 00:28:25 -0700

    Added new private data type
    private ArrayList<Person> users = new ArrayList<>(); was added along with getters and setters. Fake users were also created for testing

--- ChatSystem/src/Server/Server.java
+++ ChatSystem/src/Server/Server.java
@@ -9,9 +9,18 @@ public class Server {
 	
 	private int port;
 	private ArrayList<ServerThread> serverThreads = new ArrayList<>();
+	private ArrayList<Person> users = new ArrayList<>();
 
 	public Server(int port) {
 		this.port = port;
+	}
+
+	public ArrayList<Person> getUsers() {
+		return users;
+	}
+
+	public void setUsers(ArrayList<Person> users) {
+		this.users = users;
 	}
 
 	public int getPort() {
@@ -34,6 +43,11 @@ public class Server {
 		// TODO Auto-generated method stub
 
 		ServerSocket server;
+		
+		//FAKE USERS FOR TESTING
+		users.add(new Person("user1","pass1","it"));
+		users.add(new Person("user2","pass2","regular"));
+		users.add(new Person("","","testuser"));
 
 		try {
 
