fc91c9abbadd6ce2de5bc48b68153c5af60dfc6d
JacobLe2001 <94649389+JacobLe2001@users.noreply.github.com>
2022-04-29 19:02:18 -0700

    Update ITtest.java

--- ChatSystem/src/ITtest.java
+++ ChatSystem/src/ITtest.java
@@ -1,41 +1,50 @@
-package Server;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
 
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
+
+import Server.IT;
+import Server.Server;
 
 public class ITtest {
 	//Test to see if something is returned when the get convo data is called to retrieve log
 	@Test
 	public void getChatLogTest() {
-		assertNotNull(getConversationData());
+		IT techPerson = new IT("Temp", "Temp Pass", "it");
+		assertNotNull(techPerson.getChatLog());
 	}
 	
 	//Test to create user and check if the user is in the profiles
 	@Test
-	public void createUserTest() {
+	public void createUserTest() throws FileNotFoundException {
+		Server server = null;
+		server.readProfiles();
+		IT techPerson = new IT("Temp", "Temp Pass", "it");
 		boolean isInProfiles = false;
-		createUser("jacobTest", "jacobTestPassword", "IT");
-		for(int i = 0; i < profiles.size(); i++) {
-			if(profiles[i].getName().equals("jacobTest")) {
+		techPerson.createUser("jacobTest", "jacobTestPassword", "IT");
+		for(int i = 0; i < server.getProfiles().size(); i++) {
+			if(server.getProfiles().get(i).getUsername().equals("jacobTest")) {
 				isInProfiles = true;
 			}
 		}
 		assertTrue(isInProfiles);
-		deleteUser("jacobTest");
+		techPerson.deleteUser("jacobTest");
 	}
 	
 	//Test to create user and delete user then check if the user is in the profile
 	@Test
-	public void deleteUserTest() {
+	public void deleteUserTest() throws FileNotFoundException {
+		Server server = null;
+		server.readProfiles();
+		IT techPerson = new IT("Temp", "Temp Pass", "it");
 		boolean isInProfiles = false;
-		createUser("jacobTest", "jacobTestPassword", "IT");
-		deleteUser("jacobTest");
-		for(int i = 0; i < profiles.size(); i++) {
-			if(profiles[i].getUsername().equals("jacobTest")) {
+		techPerson.createUser("jacobTest", "jacobTestPassword", "IT");
+		techPerson.deleteUser("jacobTest");
+		for(int i = 0; i < server.getProfiles().size(); i++) {
+			if(server.getProfiles().get(i).getUsername().equals("jacobTest")) {
 				isInProfiles = true;
 			}
 		}
