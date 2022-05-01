8f23543aba1e141393414a7769b03f2000ae99be
JacobLe2001 <94649389+JacobLe2001@users.noreply.github.com>
2022-05-01 11:00:38 -0700

    Update ITWindow.java

--- ChatSystem/src/Client/ITWindow.java
+++ ChatSystem/src/Client/ITWindow.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
