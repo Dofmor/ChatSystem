03ab6465420bbec2384586df06d4a27a80fe7280
JacobLe2001 <94649389+JacobLe2001@users.noreply.github.com>
2022-05-03 15:30:33 -0700

    Update ClientDriver.java

--- ChatSystem/src/ClientDriver.java
+++ ChatSystem/src/ClientDriver.java
@@ -9,12 +9,13 @@ class ClientDriver {
 	
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
 		
-		System.out.println("Enter ip address: ");
-		Scanner input = new Scanner(System.in);
+//		System.out.println("Enter ip address: ");
+//		Scanner input = new Scanner(System.in);
 		
-		String ip = input.nextLine();
-		ip = ip.trim();
-		Client client = new Client(ip);
+//		String ip = input.nextLine();
+//		ip = ip.trim();
+		//Enter in own IP address when starting client to connect mutlitple clients
+		Client client = new Client("10.0.0.160");
 		client.run();
 		
 	}
