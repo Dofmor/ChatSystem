8ffac3028512ce0daa47b8c606525a5bb7692ad6
JacobLe2001 <94649389+JacobLe2001@users.noreply.github.com>
2022-05-03 15:32:47 -0700

    Update ServerDriver.java

--- ChatSystem/src/ServerDriver.java
+++ ChatSystem/src/ServerDriver.java
@@ -4,11 +4,11 @@ import Server.*;
 
 public class ServerDriver {
 	public static void main(String[] args) {
-//		System.out.println("Enter an IP address: ");
-//		Scanner input = new Scanner(System.in);
-//		String ip = input.nextLine();
-//		ip = ip.trim();
-//Enter in own IP address when starting server to connect mutlitple clients
+		System.out.println("Enter an IP address: ");
+		Scanner input = new Scanner(System.in);
+		String ip = input.nextLine();
+		ip = ip.trim();
+
 		Server server = new Server(7777, "10.0.0.210");
 		server.run();
 	}
