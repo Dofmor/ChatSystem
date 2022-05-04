f1b74e99972f9c703d419cd5ed8000277de01f29
JacobLe2001 <94649389+JacobLe2001@users.noreply.github.com>
2022-05-03 20:11:36 -0700

    Add files via upload

---
+++ ChatSystem/src/Testing/AllTestsProject.java
@@ -0,0 +1,13 @@
+package Testing;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({
+	ITtest.class
+})
+public class AllTestsProject {
+
+}
---
+++ ChatSystem/src/Testing/TestRunnerProject.java
@@ -0,0 +1,15 @@
+package Testing;
+import org.junit.runner.JUnitCore;
+
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunnerProject {
+   public static void main(String[] args) {
+	  Result result = JUnitCore.runClasses(AllTestsProject.class);
+	  for (Failure failure : result.getFailures()) {
+	     System.out.println(failure.toString());
+	  }
+	  System.out.println(result.wasSuccessful());
+   }
+}
